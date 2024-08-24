9790b5296e06b29549964982b0d9830a25cdeeeb
Md Fardeen <fardeenmohd1251@example.com>
2024-08-24 10:47:20 +0530

    added better navigation

--- frontend/src/components/MapInteraction.jsx
+++ frontend/src/components/MapInteraction.jsx
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Polyline, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
 
 const carIcon = new L.DivIcon({
-  html: `<div style="font-size: 24px;">🚗</div>`,
+  html: `<div style="font-size: 48px; transform: scaleX(-1);">🚗</div>`,
   className: "car-icon",
-  iconSize: [24, 24],
-  iconAnchor: [12, 12],
+  iconSize: [48, 48],
+  iconAnchor: [55,55],
 });
+
+const MapClickHandler = ({ setClickedPosition }) => {
+  useMapEvents({
+    click: (e) => {
+      const { lat, lng } = e.latlng;
+      setClickedPosition([lat, lng]);
+      console.log("Clicked location:", [lat, lng]);
+    },
+  });
+  return null;
+};
 
 const MyLeafletMap = () => {
   const [vehiclePosition, setVehiclePosition] = useState([17.385044, 78.486671]);
   const [data, setData] = useState([]);
   const [route, setRoute] = useState([[17.385044, 78.486671]]);
+  const [clickedPosition, setClickedPosition] = useState(null);
 
   const api = import.meta.env.VITE_REACT_APP_API_KEY;
 
@@ -38,7 +50,7 @@ const MyLeafletMap = () => {
     const interval = setInterval(() => {
       if (data.length > 0) {
         if (count >= data.length) {
-          count = 0; // reseting the count value to zero 
+          count
         }
 
         const { latitude, longitude } = data[count];
@@ -56,8 +68,6 @@ const MyLeafletMap = () => {
     return () => clearInterval(interval);
   }, [data]);
 
- 
-
   return (
     <MapContainer
       center={vehiclePosition}
@@ -68,8 +78,14 @@ const MyLeafletMap = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <MapClickHandler setClickedPosition={setClickedPosition} />
       <Marker position={vehiclePosition} icon={carIcon} />
       <Polyline positions={route} color="blue" />
+      {clickedPosition && (
+        <Marker position={clickedPosition}>
+          <div style={{ color: "red" }}>Clicked here!</div>
+        </Marker>
+      )}
     </MapContainer>
   );
 };
